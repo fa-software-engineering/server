4fca4a5be151af3c39d908189763817c397b1e3e
serguni <serg10101_2001@mail.ru>
2022-10-25 02:13:45 +0300

    Migrate Quarkus

--- src/main/java/ru/fa/engineering/server/dbms/entities/SuperEntity.java
+++
@@ -1,7 +0,0 @@
-package ru.fa.engineering.server.dbms.entities;
-
-import java.io.Serializable;
-
-public interface SuperEntity<IdType extends Serializable>
-        extends Auditable, Identity<IdType> {
-}
